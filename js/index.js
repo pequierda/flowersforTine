249900dbb307d6c2fa4114d007d2b4679ea6818d
emilpro <33918096+pequierda@users.noreply.github.com>
2026-04-24 12:44:34 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `For ADII`.split('')
+const text = `For ADI`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
